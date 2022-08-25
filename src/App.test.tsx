a20f6297d0198afd9f3fb6b90b30e1a486bf3cdb
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-08-25 13:46:57 +0300

    Test refactored

---
+++ src/App.test.tsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+test('renders react app', () => {
+    const { getByText } = render(
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>,
+    );
+
+    expect(getByText('Главная')).toBeInTheDocument();
+});
