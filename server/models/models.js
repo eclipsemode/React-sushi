142f500201277a510508ba1c886a42f581f752e9
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-08-27 00:04:52 +0300

    BACKEND | Models and relations created

---
+++ server/models/models.js
@@ -0,0 +1,51 @@
+const sequelize = require("../db");
+const { DataTypes } = require("sequelize");
+
+const User = sequelize.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: "USER" }
+});
+
+const Basket = sequelize.define("basket", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
+});
+
+const BasketProduct = sequelize.define("basket_product", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }
+});
+
+const Product = sequelize.define("product", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true },
+  price: { type: DataTypes.INTEGER, allowNull: false },
+  description: { type: DataTypes.STRING },
+  image: { type: DataTypes.STRING, allowNull: false }
+});
+
+const Category = sequelize.define("category", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: { type: DataTypes.STRING, unique: true, allowNull: false }
+});
+
+User.hasOne(Basket);
+Basket.belongsTo(User);
+
+Basket.hasMany(BasketProduct);
+BasketProduct.belongsTo(Basket);
+
+Category.hasMany(Product);
+Product.belongsTo(Category);
+
+Product.hasMany(BasketProduct);
+BasketProduct.belongsTo(Product);
+
+module.exports = {
+  User,
+  Basket,
+  BasketProduct,
+  Category,
+  Product
+
+};
