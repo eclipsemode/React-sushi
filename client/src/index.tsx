9fe9fc6a28e481cfb75d19e22d1c75847c18d908
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-08-29 15:39:47 +0300

    HashRouter for deploy

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,15 +2,15 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
