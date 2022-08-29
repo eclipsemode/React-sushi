744ccfbed9d0c5d3051db4054e05baa811ad8168
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-08-29 15:50:50 +0300

    preloadedState deleted

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,19 +1,20 @@
+// @ts-nocheck
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 
 import filter from './features/filterSlice';
 import products from './features/productsSlice';
 import cart, { CartStateType } from './features/cartSlice';
 
-const persistedStateCart: CartStateType = localStorage.getItem('cart')
-    ? JSON.parse(localStorage.getItem('cart') || '')
-    : {};
+// const persistedStateCart: CartStateType = localStorage.getItem('cart')
+//     ? JSON.parse(localStorage.getItem('cart') || '')
+//     : {};
 
 export const store = configureStore({
   reducer: {filter, products, cart},
-  preloadedState: { cart: persistedStateCart },
+  // preloadedState: { cart: persistedStateCart },
 });
 
-store.subscribe(() => localStorage.setItem('cart', JSON.stringify(store.getState().cart)));
+// store.subscribe(() => localStorage.setItem('cart', JSON.stringify(store.getState().cart)));
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
