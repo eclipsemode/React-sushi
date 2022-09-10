7283827ac7f1656f92383156a4c7fbd29246cd1d
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-09-10 17:34:40 +0300

    BACKEND | authorisation with jwt token

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,12 +1,49 @@
 const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { User, Basket} = require('../models/models');
 
 class UserController {
-  async registration(req, res) {
+  async registration(req, res, next) {
+    const  { email, password, role } = req.body;
 
+    if (!email || !password) {
+      return next(ApiError.badRequest('Неккоректный email или пароль.'))
+    }
+
+    const candidate = await User.findOne({where: {email}});
+
+    if (candidate) {
+      return next(ApiError.badRequest('Пользователь с таким email уже существует.'))
+    }
+
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({email, password: hashPassword, role});
+    const basket = await Basket.create({userId: user.id});
+    const token = jwt.sign(
+      {id: user.id, email: user.email, role: user.role},
+      process.env.SECRET_KEY,
+      {expiresIn: '24h'}
+    );
+    return res.json({token})
   }
 
-  async login(req, res) {
-
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await User.findOne({where: {email}});
+    if (!user) {
+      return next(ApiError.internal('Пользователь с таким email не найден.'))
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal('Указан неверный пароль.'))
+    }
+    const token = jwt.sign(
+      {id: user.id, email: user.email, role: user.role},
+      process.env.SECRET_KEY,
+      {expiresIn: '24h'}
+    );
+    return res.json({token});
   }
 
   async auth(req, res, next) {
