831e77ca21c8d75fb89959c5f2204b0350d2f0da
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-09-23 18:54:27 +0300

    FRONTEND | Header info design

---
+++ client/src/components/ModalAccount/ModalAccaunt.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './ModalAccaunt.module.css'
+
+const ModalAccount: React.FC = () => {
+  return (
+    <div className={styles.root}>
+
+    </div>
+  );
+};
+
+export default ModalAccount;
