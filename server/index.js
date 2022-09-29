667777a6644bca709ca7a4ac7c8655c3cdc00c06
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-09-29 19:43:42 +0300

    BACKEND | products added

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
 app.use('/api', router);
 
