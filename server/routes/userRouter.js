7018dc99e3ca605d3d3662fd364939fc3a724018
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-11-22 22:38:15 +0300

    BACKEND | registration schema

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -2,29 +2,9 @@ const Router = require("express");
 const router = new Router();
 const userController = require("../controllers/UserController");
 const authMiddleware = require("../middleware/authMiddleware");
-const { check } = require("express-validator");
+const registrationSchema = require("../validation/registration-schema");
 
-router.post("/registration",
-  [
-    check('email')
-      .isEmail()
-      .withMessage('Неправильный email.')
-      .normalizeEmail(),
-    check('password')
-      .isLength({ min: 8, max: 16 })
-      .withMessage('Пароль должен быть не меньше 8 и не больше 16 символов.')
-      .matches(/\d/)
-      .withMessage("Пароль должен иметь по меньшей мере 1 цифру."),
-    check('name')
-      .isLength({ min: 2, max: 10 })
-      .withMessage('Имя должно содержать не меньше 2 и не больше 10 символов.')
-      .trim(),
-    check('surname')
-      .isLength({ min: 2, max: 20 })
-      .withMessage('Фамилия должна содержать не меньше 2 и не больше 20 символов.')
-      .trim(),
-  ],
-  userController.registration);
+router.post("/registration", registrationSchema, userController.registration);
 router.get("/activate/:link", userController.activate);
 router.get("/refresh", userController.refresh);
 router.post("/login", userController.login);
