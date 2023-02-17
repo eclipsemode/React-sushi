b9e9ce5defe11afa6af33c661f8dd8a15b29e6c9
Daniel Volkonesko <eclipsemod555@gmail.com>
2023-02-17 22:26:20 +0300

    FRONTEND | delivery time

--- client/src/features/order/ui/DeliveryForm/index.tsx
+++ client/src/features/order/ui/DeliveryForm/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from './index.module.css'
+import styles from "./index.module.css";
 import BlockForm from "../BlockForm";
 import SimpleButton from "shared/UI/SimpleButton";
 import InputMask from "react-input-mask";
@@ -13,7 +13,7 @@ interface IDeliveryFormProps {
   clickEvent: () => void;
 }
 
-type PaymentType = 'cash' | 'card';
+type PaymentType = "cash" | "card";
 type TelType = `+${number} (${number}${number}${number}) ${number}${number}${number}-${number}${number}-${number}${number}`;
 type DeliveryTimeType = 1 | 2;
 
@@ -25,7 +25,7 @@ interface IFormInputs {
   room: number,
   tel: TelType,
   email: string,
-  day: 'today',
+  day: "today",
   time: string[],
   utensils: number,
   payment: PaymentType,
@@ -40,8 +40,8 @@ const DeliveryForm: React.FC<IDeliveryFormProps> = (props) => {
   const [timeStamps] = React.useState<string[]>(calcTime(15));
 
   React.useEffect(() => {
-    setValue('deliveryTime', 1);
-  }, [setValue])
+    setValue("deliveryTime", 1);
+  }, [setValue]);
 
   const onSubmit: SubmitHandler<IFormInputs> = data => {
     if (agreement) {
@@ -59,28 +59,39 @@ const DeliveryForm: React.FC<IDeliveryFormProps> = (props) => {
         <div className={styles.root__content}>
           <fieldset>
             <label className={styles.root__required}>Имя</label>
-            <input className={styles.root__input + ' ' + (errors.name && styles.root__input_invalid)} {...register('name', { required: true, maxLength: 16 })} />
+            <input
+              className={styles.root__input + " " + (errors.name && styles.root__input_invalid)} {...register("name", {
+              required: true,
+              maxLength: 16
+            })} />
           </fieldset>
           <fieldset>
             <label className={styles.root__required}>Адрес</label>
-            <input className={styles.root__input + ' ' + (errors.name && styles.root__input_invalid)} {...register('address', { required: true, maxLength: 24 })} />
+            <input
+              className={styles.root__input + " " + (errors.name && styles.root__input_invalid)} {...register("address", {
+              required: true,
+              maxLength: 24
+            })} />
           </fieldset>
           <fieldset className={styles.root__width_33}>
             <label>Подъезд</label>
-            <input className={styles.root__input + ' ' + (errors.name && styles.root__input_invalid)} {...register('entrance', {  maxLength: 2 })} />
+            <input
+              className={styles.root__input + " " + (errors.name && styles.root__input_invalid)} {...register("entrance", { maxLength: 2 })} />
           </fieldset>
           <fieldset className={styles.root__width_33}>
             <label>Этаж</label>
-            <input className={styles.root__input + ' ' + (errors.name && styles.root__input_invalid)} {...register('floor', {  maxLength: 2 })} />
+            <input
+              className={styles.root__input + " " + (errors.name && styles.root__input_invalid)} {...register("floor", { maxLength: 2 })} />
           </fieldset>
           <fieldset className={styles.root__width_33}>
             <label>Квартира</label>
-            <input className={styles.root__input + ' ' + (errors.name && styles.root__input_invalid)} {...register('room', {  maxLength: 4 })} />
+            <input
+              className={styles.root__input + " " + (errors.name && styles.root__input_invalid)} {...register("room", { maxLength: 4 })} />
           </fieldset>
           <fieldset className={styles.root__width_50}>
             <label className={styles.root__required}>Телефон</label>
             <InputMask
-              className={styles.root__input + ' ' + (errors.tel && styles.root__input_invalid)}
+              className={styles.root__input + " " + (errors.tel && styles.root__input_invalid)}
               {...register("tel", {
                 required: true,
                 pattern: /^(\+7|7|8)?[\s-]?\(?[89][0-9]{2}\)?[\s-]?[0-9]{3}[\s-]?[0-9]{2}[\s-]?[0-9]{2}$/gm
@@ -92,48 +103,61 @@ const DeliveryForm: React.FC<IDeliveryFormProps> = (props) => {
           <fieldset className={styles.root__width_50}>
             <label>E-MAIL</label>
             <input
-              className={styles.root__input + ' ' + (errors.email && styles.root__input_invalid)}
+              className={styles.root__input + " " + (errors.email && styles.root__input_invalid)}
               {...register("email", { pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })}
               placeholder="@" />
           </fieldset>
-          <fieldset className={styles.root__radio + ' ' + styles.root__width_50}>
+          <fieldset className={styles.root__radio + " " + styles.root__width_50}>
             <Radio register={register} watch={watch} value={1} defaultChecked={true} />
           </fieldset>
-          <fieldset className={styles.root__radio + ' ' + styles.root__width_50}>
+          <fieldset className={styles.root__radio + " " + styles.root__width_50}>
             <Radio register={register} watch={watch} value={2} />
           </fieldset>
-          <fieldset className={styles.root__width_50}>
-            <label>Заберу</label>
-            <select {...register('day', { required: true })}>
-              <option value="today" defaultChecked={true}>сегодня</option>
-            </select>
-          </fieldset>
-          <fieldset className={styles.root__width_50}>
-            <label>в</label>
-            <select {...register('time', { required: true })}>
-              {
-                timeStamps.map((value, index) => <option key={index + "-" + value} value={value} defaultChecked={true}>{value}</option>)
-              }
-            </select>
-          </fieldset>
+
+          {
+            +watch("deliveryTime") === 2 &&
+            <>
+              <fieldset className={styles.root__width_50}>
+                <label>Доставить</label>
+                <select {...register("day", { required: true })}>
+                  <option value="today" defaultChecked={true}>сегодня</option>
+                </select>
+              </fieldset>
+              <fieldset className={styles.root__width_50}>
+                <label>в</label>
+                <select {...register("time", { required: true })}>
+                  {
+                    timeStamps.map((value, index) => <option key={index + "-" + value} value={value}
+                                                             defaultChecked={true}>{value}</option>)
+                  }
+                </select>
+              </fieldset>
+            </>
+          }
+
+
           <fieldset className={styles.root__counter}>
             <label>Количество приборов</label>
             <div>
-              <IoIosRemoveCircleOutline className={utensils === 0 ? styles.root__btn_disabled : null} size={35} cursor="pointer" onClick={() => setUtensils((prevState) => prevState > 0 ? prevState - 1 : 0)}/>
-              <input {...register('utensils', { valueAsNumber: true, min: 0, max: 20 })} value={utensils} />
-              <IoIosAddCircleOutline size={35} cursor="pointer" onClick={() => setUtensils((prevState) => prevState + 1)}/>
+              <IoIosRemoveCircleOutline className={utensils === 0 ? styles.root__btn_disabled : null} size={35}
+                                        cursor="pointer"
+                                        onClick={() => setUtensils((prevState) => prevState > 0 ? prevState - 1 : 0)} />
+              <input {...register("utensils", { valueAsNumber: true, min: 0, max: 20 })} value={utensils} />
+              <IoIosAddCircleOutline size={35} cursor="pointer"
+                                     onClick={() => setUtensils((prevState) => prevState + 1)} />
             </div>
           </fieldset>
           <fieldset>
             <label>Оплата</label>
-            <select {...register('payment', { required: true })}>
+            <select {...register("payment", { required: true })}>
               <option value="cash">Наличными</option>
               <option value="card">Картой</option>
             </select>
           </fieldset>
           <fieldset>
             <label>Коментарий к заказу</label>
-            <textarea { ...register('commentary', { maxLength: 99 }) } placeholder="Пожалуйста, укажите здесь любую информацию, которая поможет курьеру быстрее доставить Вам заказ!"></textarea>
+            <textarea {...register("commentary", { maxLength: 99 })}
+                      placeholder="Пожалуйста, укажите здесь любую информацию, которая поможет курьеру быстрее доставить Вам заказ!"></textarea>
           </fieldset>
         </div>
       </BlockForm>
