b85c7f8b5a4d7f1c51ece380b3ca401738472d9d
Данил Волконеско <eclipsemod555@gmail.com>
2023-04-23 22:57:29 +0300

    styles

--- client/src/features/filter/ui/Popup.tsx
+++ client/src/features/filter/ui/Popup.tsx
@@ -1,48 +1,48 @@
 import React from 'react';
-import styles from './index.module.css';
+import styles from './index.module.scss';
 
 import { setSortType, setSortId, setSortOrder, selectFilter } from 'features/filter/api';
 import { SortNameType } from 'features/filter/model';
-import { useAppDispatch, useAppSelector } from "app/hooks";
+import { useAppDispatch, useAppSelector } from 'app/hooks';
 
 type PopupProps = {
-  popupHidden: boolean;
-  sortNames: SortNameType[];
-  handlePopup: () => void;
-  popupRef: React.RefObject<HTMLDivElement>;
+    popupHidden: boolean;
+    sortNames: SortNameType[];
+    handlePopup: () => void;
+    popupRef: React.RefObject<HTMLDivElement>;
 };
 
 const Popup: React.FC<PopupProps> = ({ popupHidden, sortNames, handlePopup, popupRef }) => {
-  const { sortId } = useAppSelector(selectFilter);
-  const dispatch = useAppDispatch();
+    const { sortId } = useAppSelector(selectFilter);
+    const dispatch = useAppDispatch();
 
-  const handleSortType = (id: number) => {
-    const sortObj = sortNames.find((obj: SortNameType) => obj.id === id);
+    const handleSortType = (id: number) => {
+        const sortObj = sortNames.find((obj: SortNameType) => obj.id === id);
 
-    if (!sortObj) return;
+        if (!sortObj) return;
 
-    dispatch(setSortId(sortObj.id));
-    dispatch(setSortType(sortObj.sortType));
-    dispatch(setSortOrder(sortObj.orderType));
+        dispatch(setSortId(sortObj.id));
+        dispatch(setSortType(sortObj.sortType));
+        dispatch(setSortOrder(sortObj.orderType));
 
-    handlePopup();
-  };
+        handlePopup();
+    };
 
-  return (
-    <div ref={popupRef} hidden={popupHidden} className={styles.root__popup}>
-      <ul>
-        {sortNames.map((obj: SortNameType) => (
-          <li
-            key={obj.id}
-            className={sortId === obj.id ? styles.root__popup_active : ''}
-            onClick={() => handleSortType(obj.id)}
-          >
-            {obj.text}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div ref={popupRef} hidden={popupHidden} className={styles.root__popup}>
+            <ul>
+                {sortNames.map((obj: SortNameType) => (
+                    <li
+                        key={obj.id}
+                        className={sortId === obj.id ? styles.root__popup_active : ''}
+                        onClick={() => handleSortType(obj.id)}
+                    >
+                        {obj.text}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Popup;
--- client/src/features/filter/ui/Sort.tsx
+++ client/src/features/filter/ui/Sort.tsx
@@ -1,45 +1,47 @@
 import React from 'react';
-import styles from './index.module.css';
+import styles from './index.module.scss';
 
 import { Popup } from 'features/filter/ui';
 import { SortNames, SortNameType } from 'features/filter/model';
 import { selectFilter } from 'features/filter/api';
-import { useAppSelector } from "app/hooks";
-import { CaretRightOutlined } from "@ant-design/icons";
+import { useAppSelector } from 'app/hooks';
+import { CaretRightOutlined } from '@ant-design/icons';
 
 const Sort: React.FC = () => {
-  const [popupHidden, setPopupHidden] = React.useState<boolean>(true);
-  const { sortId } = useAppSelector(selectFilter);
-  const popupRef = React.useRef<HTMLDivElement>(null);
+    const [popupHidden, setPopupHidden] = React.useState<boolean>(true);
+    const { sortId } = useAppSelector(selectFilter);
+    const popupRef = React.useRef<HTMLDivElement>(null);
 
-  const sortedItem = SortNames?.find((obj: SortNameType) => obj.id === sortId)?.text;
+    const sortedItem = SortNames?.find((obj: SortNameType) => obj.id === sortId)?.text;
 
-  const handlePopup = () => setPopupHidden(!popupHidden);
+    const handlePopup = () => setPopupHidden(!popupHidden);
 
-  React.useEffect(() => {
-    const handleClickOutside = (event: MouseEvent | React.PropsWithRef<any>) => {
-      if (popupRef.current && !popupRef.current.contains(event.target)) {
-        setPopupHidden(true);
-      }
-    };
+    React.useEffect(() => {
+        const handleClickOutside = (event: MouseEvent | React.PropsWithRef<any>) => {
+            if (popupRef.current && !popupRef.current.contains(event.target)) {
+                setPopupHidden(true);
+            }
+        };
 
-    document.body.addEventListener('click', handleClickOutside);
+        document.body.addEventListener('click', handleClickOutside);
 
-    return () => {
-      document.body.removeEventListener('click', handleClickOutside);
-    };
-  }, []);
+        return () => {
+            document.body.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
 
-  return (
-    <div className={styles.root} ref={popupRef}>
-      <div className={styles.root__label}>
-        <CaretRightOutlined rotate={popupHidden ? 0 : 90} />
-        <b>Сортировка по:</b>
-        <span className={styles.root__link} onClick={() => handlePopup()}>{sortedItem}</span>
-      </div>
-      <Popup popupHidden={popupHidden} sortNames={SortNames} handlePopup={handlePopup} popupRef={popupRef} />
-    </div>
-  );
+    return (
+        <div className={styles.root} ref={popupRef}>
+            <div className={styles.root__label}>
+                <CaretRightOutlined rotate={popupHidden ? 0 : 90} />
+                <b>Сортировка по:</b>
+                <span className={styles.root__link} onClick={() => handlePopup()}>
+                    {sortedItem}
+                </span>
+            </div>
+            <Popup popupHidden={popupHidden} sortNames={SortNames} handlePopup={handlePopup} popupRef={popupRef} />
+        </div>
+    );
 };
 
 export default Sort;
