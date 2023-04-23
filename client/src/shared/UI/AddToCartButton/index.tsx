fdfc4dc8dfbb59164db39492eb2e4ae4df46b110
Данил Волконеско <eclipsemod555@gmail.com>
2023-04-23 23:20:21 +0300

    products style

--- client/src/shared/UI/AddToCartButton/index.tsx
+++ client/src/shared/UI/AddToCartButton/index.tsx
@@ -1,46 +1,50 @@
-import React from "react";
-import styles from "./index.module.css";
-// import { CartButtonMinus, CartButtonPlus } from "../index";
-import { IProducts } from "entities/products";
-import { addItem, removeItem } from "entities/cart";
-import { useAppDispatch, useAppSelector } from "app/hooks";
-import { MinusCircleOutlined, PlusCircleOutlined } from "@ant-design/icons";
+import React from 'react';
+import styles from './index.module.scss';
+import { IProducts } from 'entities/products';
+import { addItem, removeItem } from 'entities/cart';
+import { useAppDispatch, useAppSelector } from 'app/hooks';
+import { MinusCircleOutlined, PlusCircleOutlined } from '@ant-design/icons';
 
 type AddToCartButtonProps = {
-  product: IProducts;
-}
+    product: IProducts;
+};
 
 const AddToCartButton: React.FC<AddToCartButtonProps> = ({ product }) => {
-  const dispatch = useAppDispatch();
-  const cartItem = useAppSelector((state) => state.cartReducer.items.find((obj: IProducts) => obj.id === product.id));
-  let amount = !cartItem ? 0 : cartItem.amount;
-  const handleAddItem = () => {
-    dispatch(addItem(product))
-  }
+    const dispatch = useAppDispatch();
+    const cartItem = useAppSelector((state) => state.cartReducer.items.find((obj: IProducts) => obj.id === product.id));
+    let amount = !cartItem ? 0 : cartItem.amount;
+    const handleAddItem = () => {
+        dispatch(addItem(product));
+    };
 
-  const handleRemoveItem = () => {
-    dispatch(removeItem(product.id));
-  }
+    const handleRemoveItem = () => {
+        dispatch(removeItem(product.id));
+    };
 
-  const handleAddProduct = () => {
-    dispatch(addItem(product));
-  };
+    const handleAddProduct = () => {
+        dispatch(addItem(product));
+    };
 
-  return (
-    amount > 0 ? (
-      <div className={styles.root__amountBlock}>
-        <MinusCircleOutlined style={{fontSize: '24px'}} className={styles.root__minus} onClick={() => handleRemoveItem()} />
-        <span>{amount}</span>
-        <PlusCircleOutlined style={{fontSize: '24px'}} className={styles.root__plus} onClick={() => handleAddItem()} />
-      </div>
+    return amount > 0 ? (
+        <div className={styles.root__amountBlock}>
+            <MinusCircleOutlined
+                style={{ fontSize: '24px' }}
+                className={styles.root__minus}
+                onClick={() => handleRemoveItem()}
+            />
+            <span>{amount}</span>
+            <PlusCircleOutlined
+                style={{ fontSize: '24px' }}
+                className={styles.root__plus}
+                onClick={() => handleAddItem()}
+            />
+        </div>
     ) : (
-      <button className={styles.root} onClick={() => handleAddProduct()}>
-        <span>Добавить</span>
-        <span>{amount === 0 ? "+" : "x" + amount}</span>
-      </button>
-    )
-  );
-
+        <button className={styles.root} onClick={() => handleAddProduct()}>
+            <span>Добавить</span>
+            <span>{amount === 0 ? '+' : 'x' + amount}</span>
+        </button>
+    );
 };
 
 export default AddToCartButton;
--- client/src/widgets/Products/Item/Item.tsx
+++ client/src/widgets/Products/Item/Item.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import styles from './Item.module.css';
+import styles from './Item.module.scss';
 import { IProducts } from 'entities/products';
 import { AddToCartButton } from 'shared/UI';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Item: React.FC<IProducts> = ({ ...product }) => {
     const [imgScale, setImgScale] = React.useState<boolean>(false);
@@ -12,23 +12,23 @@ const Item: React.FC<IProducts> = ({ ...product }) => {
 
     return (
         <div onMouseEnter={() => handleOnMouseOver()} onMouseLeave={() => handleOnMouseLeave()}>
-          <Link className={styles.root} to="">
-            <img
-                className={styles.image + ' ' + (imgScale ? styles.image__resize : null)}
-                src={process.env.REACT_APP_API_URL + product.image}
-                alt="Item"
-            />
-            <div className={styles.root__content}>
-                <h4 className={styles.title}>{product.name}</h4>
-                <div className={styles.root__description}>
-                    <p>{product.description}</p>
+            <Link className={styles.root} to="">
+                <img
+                    className={styles.image + ' ' + (imgScale ? styles.image__resize : null)}
+                    src={process.env.REACT_APP_API_URL + product.image}
+                    alt="Item"
+                />
+                <div className={styles.root__content}>
+                    <h4 className={styles.title}>{product.name}</h4>
+                    <div className={styles.root__description}>
+                        <p>{product.description}</p>
+                    </div>
+                    <div className={styles.bottom}>
+                        <div className={styles.price}>{product.price} ₽</div>
+                        <AddToCartButton product={product} />
+                    </div>
                 </div>
-                <div className={styles.bottom}>
-                    <div className={styles.price}>{product.price} ₽</div>
-                    <AddToCartButton product={product} />
-                </div>
-            </div>
-          </Link>
+            </Link>
         </div>
     );
 };
