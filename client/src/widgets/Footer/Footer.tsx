80f6601bf9301c90095f1dd87d725823a714d304
Данил Волконеско <eclipsemod555@gmail.com>
2023-04-25 21:42:26 +0300

    footer adaptive

--- client/src/widgets/Footer/Footer.tsx
+++ client/src/widgets/Footer/Footer.tsx
@@ -8,9 +8,10 @@ import { Link } from 'react-router-dom';
 import RouterPath from '../../app/utils/menuPath';
 import { useAppSelector } from '../../app/hooks';
 import { selectCity } from '../../entities/city';
+import { DeviceType, selectAdaptiveServiceSlice } from '../../processes/services/adaptiveService/adaptiveService';
 
 const Footer: React.FC = () => {
-    // @ts-ignore
+    const { deviceType } = useAppSelector(selectAdaptiveServiceSlice);
     const { categories, categoriesStatus } = useAppSelector((state) => state.categoriesReducer);
     const { city } = useAppSelector(selectCity);
 
@@ -18,10 +19,23 @@ const Footer: React.FC = () => {
         <footer id="footer" className={styles.root}>
             <div className="container">
                 <Stack textAlign="center" justifyContent="space-between" spacing={2}>
-                    <Stack direction="row" justifyContent="space-between">
+                    <Stack
+                        direction={deviceType === DeviceType.DESKTOP ? 'row' : 'column-reverse'}
+                        justifyContent="space-between"
+                    >
                         <Stack textAlign="left" spacing={2}>
-                            <span className={styles.tel}>8 (800) 200-27-92</span>
-                            <Stack className={styles.stores} direction="row" spacing={1}>
+                            <span
+                                className={styles.tel}
+                                style={{ textAlign: deviceType === DeviceType.DESKTOP ? 'left' : 'center' }}
+                            >
+                                8 (800) 200-27-92
+                            </span>
+                            <Stack
+                                className={styles.stores}
+                                direction="row"
+                                justifyContent={deviceType === DeviceType.DESKTOP ? 'flex-start' : 'center'}
+                                spacing={1}
+                            >
                                 <a
                                     href="https://apps.apple.com/us/app/%D0%BB%D0%B0%D0%B9%D0%BC-%D0%B4%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D0%BA%D0%B0-%D1%81%D1%83%D1%88%D0%B8-%D0%B8-%D0%BF%D0%B8%D1%86%D1%86%D1%8B/id1570970073?uo=4"
                                     target="_blank"
@@ -37,7 +51,12 @@ const Footer: React.FC = () => {
                                     <img src={GooglePlayImg} width="120px" alt="google_play" />
                                 </a>
                             </Stack>
-                            <Stack direction="row" className={styles.links} spacing={2}>
+                            <Stack
+                                direction="row"
+                                className={styles.links}
+                                justifyContent={deviceType === DeviceType.DESKTOP ? 'flex-start' : 'center'}
+                                spacing={2}
+                            >
                                 <Link to={RouterPath.HOME}>Главная</Link>
                                 <Link to={RouterPath.HOME}>Контакты</Link>
                             </Stack>
@@ -49,7 +68,10 @@ const Footer: React.FC = () => {
                             </span>
                             <span>Доставка с 10:00–23:30</span>
                         </Stack>
-                        <Stack className={styles.categories}>
+                        <Stack
+                            className={styles.categories}
+                            display={deviceType === DeviceType.DESKTOP ? 'auto' : 'none'}
+                        >
                             {categoriesStatus === 'fulfilled'
                                 ? categories.map((category) => (
                                       <Link to={`${RouterPath.CATEGORY}` + category.id} key={category.id}>
