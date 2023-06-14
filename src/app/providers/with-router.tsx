a5feaaf4f4faaab24795d24222f0909231605644
Данил Волконеско <eclipsemod555@gmail.com>
2023-06-14 21:42:27 +0300

    loader

--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -3,10 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 import { PulseLoader } from "react-spinners";
 const withRouter = (component: () => React.ReactNode) => () => {
   const loader = (
-    <div style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <PulseLoader color="#8ba4f9" />
-    </div>
+      <div style={{ width: "100vw", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <PulseLoader color="#4caf50" />
+      </div>
   );
+
   return (
     <BrowserRouter>
     <React.Suspense fallback={loader}>
