b3204732547306ab974e9e02d25e71b067f152a9
Данил Волконеско <eclipsemod555@gmail.com>
2023-07-01 01:08:07 +0300

    delete file

--- src/shared/utils/formatToDate.ts
+++
@@ -1,7 +0,0 @@
-function formatToDate(date: string) {
-  const dateArr: string[] = date.split('-');
-  [dateArr[0], dateArr[1]] = [dateArr[1], dateArr[0]];
-  return new Date(dateArr.join('-'));
-}
-
-export default formatToDate;
